ac4598d68c66d314e26e995e22c4c47fa0afbf80
Igor Budnitski <igor.budnitski@gmail.com>
2026-08-22 09:22:22 +0300

    Comments

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const http = require('http')
 let reqCounter = 0;
 
 const server = http.createServer((request, response)=> {
+
+    // If request URL NOT equal to favicon then increase the counter
     if (request.url !== '/favicon.ico'){
         reqCounter++;
     }
 
+    // switching between different routes
     switch (request.url){
         case '/':
             response.write('Home page.\n')
